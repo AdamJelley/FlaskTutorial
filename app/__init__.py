d75b024dfd426a95d598ed750765f511d6b3a5e8
Adam Jelley <47476156+AdamJelley@users.noreply.github.com>
2020-09-12 13:03:26 +0100

    Added error pages for 404 and 500 HTTP codes

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,8 @@ if not app.debug:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no_reply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'], subject='Microblog failure',
